39bb9de97fca8ac68408a7d957f74371f923d171
klealufide <kleal40321@ufide.ac.cr>
2025-06-12 20:04:53 -0600

    Semana 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,135 +1,195 @@
-let nombre = "Karol Leal";
-let edad = 35;
-let estatura = 1.60;
-let esEstudiante = false;
+document.addEventListener("DOMContentLoaded", function () {
+    let nombre = "Karol Leal";
+    let edad = 35;
+    let estatura = 1.60;
+    let esEstudiante = false;
 
-console.log(nombre);
-console.log(edad);
-console.log(estatura);
-console.log(esEstudiante);
+    let mensajeSpan = document.getElementById("mensaje");
+    mensajeSpan.style.display = "none";
 
-let total = 0;
+    console.log(nombre);
+    console.log(edad);
+    console.log(estatura);
+    console.log(esEstudiante);
 
-console.log(total);
+    let total = 0;
 
-function suma(a, b) {
-    total = a + b;
     console.log(total);
-}
 
-suma(2, 4);
-console.log(total);
+    function suma(a, b) {
+        total = a + b;
+        console.log(total);
+    }
 
-console.log(nombre.length);
-console.log(nombre.replaceAll("o", "0"));
-console.log(nombre.endsWith("eal"));
+    suma(2, 4);
+    console.log(total);
 
-console.log(edad.toString());
-console.log(estatura.toFixed(1));
+    console.log(nombre.length);
+    console.log(nombre.replaceAll("o", "0"));
+    console.log(nombre.endsWith("eal"));
 
-let mensaje = edad >= 18 ? "SI es mayor de edad" : "NO es mayor de edad";
-console.log(mensaje);
-edad = 10;
-if (edad >= 18) {
-    console.log("Es mayor de edad");
-}
+    console.log(edad.toString());
+    console.log(estatura.toFixed(1));
 
-let nota = 50;
+    let mensaje = edad >= 18 ? "SI es mayor de edad" : "NO es mayor de edad";
+    console.log(mensaje);
+    edad = 10;
+    if (edad >= 18) {
+        console.log("Es mayor de edad");
+    }
 
-if (nota >= 70) {
-    console.log("Aprobo");
-} else {
-    console.log("reprobo");
-}
+    let nota = 50;
 
-//if, else if, else if else -> switch
+    if (nota >= 70) {
+        console.log("Aprobo");
+    } else {
+        console.log("reprobo");
+    }
 
-if (nota >= 80) {
-    console.log("Nota Excelente");
-} else if (nota <= 79 && nota >= 60) {
-    console.log("debes mejorar");
-} else {
-    console.log("repruebas")
-}
+    //if, else if, else if else -> switch
+
+    if (nota >= 80) {
+        console.log("Nota Excelente");
+    } else if (nota <= 79 && nota >= 60) {
+        console.log("debes mejorar");
+    } else {
+        console.log("repruebas")
+    }
 
 
-let semaforo = "rojo";
+    let semaforo = "rojo";
 
-if (semaforo == "rojo") {
-    console.log("detengase");
-} else if (semaforo == "verde") {
-    console.log("siga");
-} else if (semaforo == "amarillo") {
-    console.log("precaucion");
-} else {
-    console.log("COlor no existe");
-}
-
-switch (semaforo) {
-    case 'rojo':
+    if (semaforo == "rojo") {
         console.log("detengase");
-        break;
-    case 'verde':
+    } else if (semaforo == "verde") {
         console.log("siga");
-        break;
-    case 'amarillo':
+    } else if (semaforo == "amarillo") {
         console.log("precaucion");
-        break;
-    default:
+    } else {
         console.log("COlor no existe");
-        break;
-}
+    }
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
+    switch (semaforo) {
+        case 'rojo':
+            console.log("detengase");
+            break;
+        case 'verde':
+            console.log("siga");
+            break;
+        case 'amarillo':
+            console.log("precaucion");
+            break;
+        default:
+            console.log("COlor no existe");
+            break;
+    }
 
-
-for (let i = 0; i <= 10; i++) {
-    console.log("Tabla 4 * " + i + " = " + (4 * i));
-}
-
-let listaFrutas = [
-    'melon',
-    "sandia",
-    "papaya"
-];
-
-for (let i = 0; i < listaFrutas.length; i++) {
-    console.log(listaFrutas[i]);
-}
-
-for (x in listaFrutas) {
-    console.log(listaFrutas[x]);
-}
-
-edad = 49;
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
 
 
-while (edad < 50) {
-    edad += 1;
-    console.log("WHILE" + edad);
-}
-// al menos 1 vez
+    for (let i = 0; i <= 10; i++) {
+        console.log("Tabla 4 * " + i + " = " + (4 * i));
+    }
+
+    let listaFrutas = [
+        'melon',
+        "sandia",
+        "papaya"
+    ];
+
+    for (let i = 0; i < listaFrutas.length; i++) {
+        console.log(listaFrutas[i]);
+    }
+
+    for (x in listaFrutas) {
+        console.log(listaFrutas[x]);
+    }
+
+    edad = 49;
 
 
-do {
-    edad += 1;
-    console.log("DOWHILE" + edad);
-} while (edad < 50)
+    while (edad < 50) {
+        edad += 1;
+        console.log("WHILE" + edad);
+    }
+    // al menos 1 vez
 
 
-function sumar(a,b){
-    return a+b;
-}
+    do {
+        edad += 1;
+        console.log("DOWHILE" + edad);
+    } while (edad < 50)
 
-let resultado = sumar(4,5);
-console.log(resultado);
 
-console.log(sumar(3,4));
+    function sumar(a, b) {
+        return a + b;
+    }
 
-function saludar(nombre){
-    console.log("Hola!! "+nombre);
-}
+    let resultado = sumar(4, 5);
+    console.log(resultado);
 
-saludar("karol");
+    console.log(sumar(3, 4));
+
+    function saludar(nombre) {
+        console.log("Hola!! " + nombre);
+    }
+
+    saludar("karol");
+
+
+    let titulo = document.getElementById("titulo");
+
+    titulo.innerText = "Semana 05";
+    titulo.style.color = "red";
+
+    let subtitulo = document.getElementById("tituloTareas");
+    subtitulo.style.color = "blue";
+
+    console.log(titulo);
+
+    let listaTareas = document.getElementById("listaTareas");
+    let nuevaTarea = document.createElement("li");
+    nuevaTarea.innerText = "Tarea 4";
+    listaTareas.appendChild(nuevaTarea);
+
+    let seccionImagen = document.getElementById("seccionImagen");
+    let nuevoSubtitulo = document.createElement("h2");
+    nuevoSubtitulo.innerText = "Seccion de imagen";
+    seccionImagen.appendChild(nuevoSubtitulo);
+
+    let link = document.getElementById("link");
+    link.setAttribute("href", "https://www.fidelitasvirtual.org/");
+
+    let imagen = document.getElementById("imagen");
+
+    imagen.addEventListener("mouseenter", function () {
+        imagen.setAttribute("src", "./img/salon.jpg");
+    })
+
+
+    imagen.addEventListener("mouseleave", function () {
+        imagen.setAttribute("src", "./img/img1.jpg");
+    })
+
+    let agregarTareaBtn = document.getElementById("agregarTarea");
+
+    agregarTareaBtn.addEventListener("click", function () {
+        let nuevaTarea = document.getElementById("nuevaTarea");
+        if (nuevaTarea.value != "") {
+            let nuevaTareaElmento = document.createElement("li");
+            nuevaTareaElmento.innerText = nuevaTarea.value;
+            listaTareas.appendChild(nuevaTareaElmento);
+            nuevaTarea.value = "";
+            mensajeSpan.style.display = "none";
+            nuevaTarea.style.borderColor = "black";
+        } else {
+            mensajeSpan.style.display = "block";
+            nuevaTarea.style.borderColor = "red";
+        }
+
+    });
+
+
+});
