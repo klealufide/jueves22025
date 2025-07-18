f2ce4da4be77c5c4b5bd79f1c18ee31694459b89
klealufide <kleal40321@ufide.ac.cr>
2025-07-17 19:48:02 -0600

    segunda parte

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     let titulo = document.getElementById("titulo");
     let tituloJQ = $("#titulo");
 
@@ -16,7 +16,26 @@ $(function() {
     let multiselector = $("#titulo, .grupo2, h2");
     console.log(multiselector);
 
-    $("#btnSaludar").on("click", function(){
-        alert("Hola!");
+    $("#btnSaludar").on("click", function () {
+        $(".grupo2").css("color", "purple");
+        $("#titulo").addClass("grupo1");
     });
+
+    $("#btnAgregarTarea").on("click", function () {
+        let tarea = $("#tarea").val();
+        if (tarea == "") {
+            $("#tarea").css("borderColor", "red");
+        } else {
+            $("#tarea").css("borderColor", "black");
+            $("#titulo").removeClass("grupo1");
+        }
+    })
+
+    $("img").on("mouseenter", function () {
+        $("img").attr("src", "img/biblioteca.jpg");
+    })
+
+    $("img").on("mouseleave", function () {
+        $("img").attr("src", "img/img1.jpg");
+    })
 })
